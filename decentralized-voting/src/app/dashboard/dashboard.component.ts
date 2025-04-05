2a52dfdd71e6c86c8c780daed0f26b01352a014a
Mrazakos <mrazakos05@gmail.com>
2025-04-05 13:23:17 +0200

    add carousel to dashboared

--- decentralized-voting/src/app/dashboard/dashboard.component.ts
+++ decentralized-voting/src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from '../auth/services/auth.service';
-import {VotingCardComponent} from '../voting-card/voting-card.component'
+import { VotingCardComponent } from '../voting-card/voting-card.component';
 import { VotingCard } from '../voting-card/model/voting-card';
-import { VotingType } from '../models/voting-type';
-import { CommonModule } from '@angular/common';
 import { VotingCardService } from '../services/voting-card.service';
 import { Observable } from 'rxjs';
+import { CarouselModule } from 'primeng/carousel'; // Import CarouselModule
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  imports: [VotingCardComponent,
-    CommonModule
-  ]
+  standalone: true, // Mark the component as standalone
+  imports: [CommonModule, VotingCardComponent, CarouselModule, MatCardModule], // Add CarouselModule here
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  votingCards$: Observable<VotingCard[]> = new Observable<VotingCard[]>();
+  numVisibleCards: number = 2; // Number of cards to show at once
 
-  votingCards$!: Observable<VotingCard[]>;
-
-  constructor(private votingCardService: VotingCardService,
-    private authService: AuthService
-  ) {}
+  constructor(private votingCardService: VotingCardService, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.votingCards$ = this.votingCardService.getVotingCards();
+    this.getNumVisibleCards();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.getNumVisibleCards();
+  }
+
+  getNumVisibleCards() {
+    if (window.innerWidth <= 780) {
+      this.numVisibleCards = 1;
+    } else if (window.innerWidth <= 1440) {
+      this.numVisibleCards = 2;
+    } else {
+      this.numVisibleCards = 3;
+    }
   }
 
   onLogout(): void {
